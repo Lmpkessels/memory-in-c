2ea689c387dad478958902fe815c683d94c316e6
lkessels <l@lmpkessels.com>
2026-02-10 12:51:41 +0100

    refactor: more readable code

--- pointers/multidimensional_arrays.c
+++ pointers/multidimensional_arrays.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+#define ROWS 5
+#define COLS 10
+
 // 2D Array.
-int characters_and_numbers[5][10] = {
+int characters_and_numbers[ROWS][COLS] = {
     {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'},
     {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'},
     {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'},
@@ -11,7 +14,7 @@ int characters_and_numbers[5][10] = {
 
 // Address arithmetic derefencing, represented as:
 // *(*(ptr_to_characters_and_number + i) + j);
-int (*ptr_to_characters_and_numbers)[10];
+int (*ptr_to_characters_and_numbers)[COLS];
 
 // Getting indises out of a 2D array using address arithmetic dereferencing.
 // 
@@ -19,15 +22,15 @@ int (*ptr_to_characters_and_numbers)[10];
 // to access each nested array in memory, then dereference to access each indice
 // in memory.
 int main(void) {
-    int i, j;
+    int row, col;
     ptr_to_characters_and_numbers = characters_and_numbers;
 
     printf("\n\n");
-    for (i = 0; i < 5; i++) {
-        for (j = 0; j < 10; j++) {
+    for (row = 0; row < 5; row++) {
+        for (col = 0; col < 10; col++) {
             printf(
                 "[%d][%d] = %c\n"
-                , i, j, *(*(ptr_to_characters_and_numbers + i) + j)
+                , row, col, *(*(ptr_to_characters_and_numbers + row) + col)
             );
         }
     }
