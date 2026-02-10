08e170b0e0ad946821a9314a2b0aa5fde74ddb6b
lkessels <l@lmpkessels.com>
2026-02-10 13:15:04 +0100

    fix: buffer overflow and length count

--- pointers/more_on_strings.c
+++ pointers/more_on_strings.c
@@ -1,25 +1,37 @@
 #include <stdio.h>
 
-char dest[] = "Hello";
-char source[] = "Hello, World!";
+char dest[40] = "Hello";
+char source[40] = "Hello, World!";
+
+size_t str_length(const char *s) {
+    size_t len = 0;
+
+    while (s[len] != '\0') {
+        len++;
+    }
+
+    return len;
+}
 
 // Instead of slicing, accessing the address manually is done here,
 // (*(source + i) access the address in memory.)
 //
 // This is called pointer notation, and it's used instead of array notation.
-char *my_strcpy(char dest[], char source[]) {
-    int i = 0;
+char *my_strcpy(char *dest, const char *source) {
+    size_t i = 0;
 
     while (*(source + i) != '\0') {
         *(dest + i) = *(source + i);
         i++;
     }
 
-    for (i = 0; i < *(dest + i); i++) {
+    *(dest + i) = '\0'; // Terminate (mark end of string).
+
+    for (i = 0; i < str_length(dest); i++) {
         printf("%d\n", *(dest + i));
     }
 
-    *(dest + i) = '\0';
+    *(dest + i) = '\0'; // Terminate (mark end of string).
     return dest;
 }
 
